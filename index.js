d2fb8b432ea551a39c397415560f1f8de733dd5d
IvyL <ivy.iu@moodys.com>
2019-03-01 18:24:35 -0500

    add org member webhook

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const{log, assignIncomment, AddMemberRequest, RemoveMemberRequest} = require('./lib')
+const{log, addMemberRequest, removeMemberRequest} = require('./lib')
 
 module.exports = robot =>{
     log(robot, 'start')
-    robot.on('membership.added', context => AddMemberRequest(robot,context))
-    robot.on('membership.removed', context => RemoveMemberRequest(robot,context))
+    robot.on('organization.member_added', context => AddMemberRequest(robot,context))
+    robot.on('organization.member_removed', context => RemoveMemberRequest(robot,context))
 }
