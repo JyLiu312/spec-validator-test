f2cb400a325f92ac52e1aa969629ae159d20074c
Ivy Liu <47038622+JyLiu312@users.noreply.github.com>
2019-03-01 18:27:01 -0500

    fix typo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const{log, addMemberRequest, removeMemberRequest} = require('./lib')
 
 module.exports = robot =>{
     log(robot, 'start')
-    robot.on('organization.member_added', context => AddMemberRequest(robot,context))
-    robot.on('organization.member_removed', context => RemoveMemberRequest(robot,context))
+    robot.on('organization.member_added', context => addMemberRequest(robot,context))
+    robot.on('organization.member_removed', context => removeMemberRequest(robot,context))
 }
